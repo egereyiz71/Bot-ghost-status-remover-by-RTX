0939498a3b0cac5e1ae2d9f4518a9ee7ca49c00f
egereyiz71 <130172372+egereyiz71@users.noreply.github.com>
2024-04-03 22:08:21 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to Infinity","Watching Infinity"];
 
 
 let currentIndex = 0;
